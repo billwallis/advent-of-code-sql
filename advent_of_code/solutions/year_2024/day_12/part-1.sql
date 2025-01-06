881585ec1062adff18dba99fd007ec059668209d
Bill <bill@thewallisfamily.co.uk>
2025-01-06 20:59:55 +0000

    ✨ feat: add SQL solution for day 12 (2024)

--- advent_of_code/solutions/year_2024/day_12/part-1.sql
+++ advent_of_code/solutions/year_2024/day_12/part-1.sql
@@ -74,72 +74,89 @@ region_edges as (
         from region_edges__right_down
 ),
 
-regions(i, region_id, region, point, seen) as (
-        select 0, uuid(), plant, [x, y], [[x, y]]
-        from grid
+/*
+    TODO  Two recursive CTEs: on regions (pick any point), then on the point to fill the region
+*/
+regions(region_id, region, region_points, seen) as materialized (
+        select
+            null::uuid,
+            null::varchar,
+            []::int[][],
+            []::int[][],
     union all (
-        with next_points as (
-            from (
-                select
-                    regions.i + 1 as i,
-                    regions.region_id,
-                    regions.region,
-                    region_edges.point_2 as point,
-                    regions.seen,
-                from regions
-                    inner join region_edges
-                        on  regions.point = region_edges.point_1
-                        and not regions.seen.list_contains(region_edges.point_2)
-            )
+        with recursive
+
+        next_region as materialized (
             select
-                i,
-                region_id,
-                region,
-                point,
-                (seen
-                    .list_concat(list(point) over (partition by region_id))
-                    .list_distinct()
-                    .list_sort()
-                ) as seen,
+                uuid() as region_id,
+                grid.plant as region,
+                [grid.x, grid.y] as point,
+                regions.seen as regions_seen,
+            from grid, regions
+            where not list_contains(regions.seen, [grid.x, grid.y])
+            limit 1
+        ),
+
+        region_points as (
+                select
+                    region_id,
+                    point,
+                    [point] as seen,
+                from next_region
+            union all
+                select
+                    region_points.region_id,
+                    region_edges.point_2,
+                    (region_points.seen
+                        .list_concat(list(region_edges.point_2) over (
+                            partition by region_points.region_id
+                        ))
+                        .list_distinct()
+                        .list_sort()
+                    ),
+                from region_points
+                    inner join region_edges
+                        on region_points.point = region_edges.point_1
+                where not list_contains(region_points.seen, region_edges.point_2)
         )
 
-        /* If region IDs overlap (intersection in `seen`), combine them */
-        from next_points
-        -- select
-        --     i,
-        --     (
-        --         select min(region_id)
-        --         from next_points as innr
-        --         /* Only one depth of intersection */
-        --         where list_intersect(next_points.seen, innr.seen) != []
-        --     ),
-        --     region,
-        --     point,
-        --     seen,
+        select
+            region_id,
+            next_region.region,
+            points.region_points,
+            list_concat(next_region.regions_seen, points.region_points),
+        from next_region
+            inner join (
+                select region_id, list(distinct point) as region_points
+                from region_points
+                group by region_id
+            ) as points
+                using (region_id)
     )
 ),
 
 region_areas as (
-    from (
-        select region_id, region, seen
-        from regions
-        qualify i = max(i) over (partition by region_id)
-    )
     select
-        seen,
-        any_value(region) as region,
-        any_value(region_id) as region_id,
-        len(seen) as area,
-    group by seen
+        region_id,
+        region,
+        region_points,
+        len(region_points) as area,
+    from regions
+    where region_id is not null
 ),
 
 region_perimeters as (
     from (
-        from region_areas
+        from (
+            from region_areas
+            select
+                region_id,
+                unnest(region_points) as point,
+        )
         select
             region_id,
-            unnest(seen)[1] as x,
-            unnest(seen)[2] as y,
+            point[1] as x,
+            point[2] as y,
     ) inner join perimeters using (x, y)
     select
         region_id,
