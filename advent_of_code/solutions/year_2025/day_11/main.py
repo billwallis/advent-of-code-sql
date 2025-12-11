3d94ae1446ada7727bef807d42ebe71f24330169
Bill Wallis <bill@tasman.ai>
2025-12-11 08:27:22 +0000

    feat: add FAILING part 2 solution for 2025 day 11

--- advent_of_code/solutions/year_2025/day_11/main.py
+++ advent_of_code/solutions/year_2025/day_11/main.py
@@ -24,11 +24,13 @@ def solution(use_sample: bool) -> list:
     """
     Solve the day 11 problem!
     """
-    file = HERE / ("sample.data" if use_sample else "input.data")
-    read_input(file)
+    file_1 = HERE / ("sample-1.data" if use_sample else "input.data")
+    file_2 = HERE / ("sample-2.data" if use_sample else "input.data")
+    read_input(file_1)
+    read_input(file_2)
 
-    part_1 = _read("part-1.sql").replace("{{ file }}", str(file.absolute()))
-    part_2 = _read("part-2.sql").replace("{{ file }}", str(file.absolute()))
+    part_1 = _read("part-1.sql").replace("{{ file }}", str(file_1.absolute()))
+    part_2 = _read("part-2.sql").replace("{{ file }}", str(file_2.absolute()))
 
     return [
         duckdb.sql(part_1).fetchone()[0],
